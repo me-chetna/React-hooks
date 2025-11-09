a58097e42bc2bfb9167825f219e113b765cdff74
me-chetna <chetna.jain.ug24@nsut.ac.in>
2025-11-09 11:44:32 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useCallback, useEffect, useState, useRef } from 'react'
+import './App.css'
+import Card from './components/card.jsx'
+
+function App() {
+  return (
+    <div>
+    <Card />
+    </div> 
+  )
+}
+export default App
