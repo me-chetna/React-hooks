46d139db5646bdd1b2e4aecdd0742975e6bffd5e
Chetna Jain <chetna.jain.ug24@nsut.ac.in>
2026-01-14 23:38:03 +0530

    add

---
+++ src/components/useFetch.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
